11a9c400044070561f421a55267b71585bb2e6ce
WixazZ <47520060+WixazZ@users.noreply.github.com>
2020-05-15 01:06:25 +0200

    début

---
+++ main.c
@@ -0,0 +1,11 @@
+//
+// Created by parad on 01/05/2020.
+//
+#include<stdio.h>
+#include "fonctions.h"
+
+int main(){
+    printf("Hello word");
+    membre* tab_membres = recup_membres_data();
+    return 0;
+}
